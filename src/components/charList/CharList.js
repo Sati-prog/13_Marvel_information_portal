8568c1ba4984e1165f4869cc82d0085ca6fc0c0b
Sati <rock_sal@mail.ru>
2022-08-09 13:47:13 +0600

    Practice with lifecycle, componentDidUpdate

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,7 +1,9 @@
 import { Component } from 'react';
+
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
+
 import './charList.scss';
 
 class CharList extends Component {
@@ -54,7 +56,8 @@ class CharList extends Component {
             return (
 
                 <li className="char__item"
-                    key={item.id}>
+                    key={item.id}
+                    onClick={() => this.props.onCharSelected(item.id)}>
                     <img src={item.thumbnail}
                          alt={item.name}
                          style={imgStyle}/>
